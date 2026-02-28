2dbca1d34f10692d7b8204f193db5af209d30c2c
Alisha Chen <alishachen@ucsb.edu>
2026-02-28 14:17:33 -0800

    a

--- main.cpp
+++ main.cpp
@@ -90,7 +90,7 @@ int main(int argc, char** argv){
 		  //  cout << endl;
                     pq.push(*it);
                     ++it;
-          `      } else {
+                } else {
                     break;
                 }
             }
